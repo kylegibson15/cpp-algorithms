e64748b903d89af05074ba76ef18c440030318c4
kylegibson15 <kylegibson15@gmail.com>
2020-11-25 00:05:08 -0700

    clean up and add cmake to the project

---
+++ build/CMakeFiles/Algorithms.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/kylegibson/Personal_Projects/C++/cpp-algorithms/src/main.cpp" "/Users/kylegibson/Personal_Projects/C++/cpp-algorithms/build/CMakeFiles/Algorithms.dir/src/main.cpp.o"
+  "/Users/kylegibson/Personal_Projects/C++/cpp-algorithms/src/multiply/multiply.cpp" "/Users/kylegibson/Personal_Projects/C++/cpp-algorithms/build/CMakeFiles/Algorithms.dir/src/multiply/multiply.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/Algorithms.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "Algorithms"
+  "Algorithms.pdb"
+  "CMakeFiles/Algorithms.dir/src/main.cpp.o"
+  "CMakeFiles/Algorithms.dir/src/multiply/multiply.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Algorithms.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
